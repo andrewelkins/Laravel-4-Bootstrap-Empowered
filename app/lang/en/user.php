216b31cee4bf348e347d92bab58a291e1a552daf
andrew13 <github@andrewelkins.com>
2013-02-23 18:09:51 -0800

    adding language file for user

---
+++ app/lang/en/user.php
@@ -0,0 +1,21 @@
+<?php 
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| User Language Lines
+	|--------------------------------------------------------------------------
+	|
+	|
+	*/
+
+	'register'          => 'Register',
+	'login'             => 'Login',
+	'account'           => 'Account',
+	'forgot_password'   => 'Forgot Password',
+	'settings'          => 'Settings',
+	'profile'          => 'Profile',
+	'user_account_is_not_confirmed'          => 'User Account is not Confirmed.',
+
+);
